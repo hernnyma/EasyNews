b1935a5ab60f749a01c43e41ea202caf20683fed
hernnyma <vanderock12@gmail.com>
2023-01-25 14:49:03 -0500

    add utilities

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import {signUp} from '../../utilities/users-service';
 
 export default class SignUpForm extends Component {
 
@@ -15,6 +16,26 @@ export default class SignUpForm extends Component {
           [evt.target.name]: evt.target.value,
           error: ''
         });
+      };
+
+      handleSubmit = async (evt) => {
+        // Prevent form from being submitted to the server
+        evt.preventDefault();
+        try {
+            const formData = {...this.state};
+            delete formData.error;
+            delete formData.confirm;
+            delete formData.error;
+            // The promise returned by the signUp service method 
+            // will resolve to the user object included in the
+            // payload of the JSON Web Token (JWT)
+            const user = await signUp(formData);
+            // Baby step!
+            console.log(user)
+        } catch {
+          // An error occurred 
+          this.setState({ error: 'Sign Up Failed - Try Again' });
+        }
       };
       
 
