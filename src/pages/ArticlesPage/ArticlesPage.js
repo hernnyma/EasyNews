b7f8a08c2b5f4d214004b03df19008227d19c574
hernnyma <vanderock12@gmail.com>
2023-01-26 10:40:26 -0500

    added puppeteer

--- src/pages/ArticlesPage/ArticlesPage.js
+++ src/pages/ArticlesPage/ArticlesPage.js
@@ -1,9 +1,10 @@
-
+import Article from '../../components/Article/Article'
 
 export default function ArticlesPage() {
     return (
       <div className="ArticlesPage">
         ArticlesPage
+        <Article/>
       </div>
     );
   }
